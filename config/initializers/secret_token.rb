6c86dbebed646f85f2273d98a3376b7a9ba7b2d6
omri-simplee <omri@simplee.com>
2017-02-02 11:00:55 -0800

    First commit for new_options app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewOptions::Application.config.secret_token = '363f72c8fc0efaec886a323559b11bb8ebfebb2ae9b1a0a525cf4c07e2d2d4898e0bab64dd30310967dcc2c478244ae8744f4b224c7c6a54695d5cd99a420702'
